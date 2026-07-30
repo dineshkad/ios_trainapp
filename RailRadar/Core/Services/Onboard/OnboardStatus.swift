5ad2e6822161db2d31a84fb23214c84350339489
dkadali <dinesh.kvdsr@gmail.com>
2026-07-30 01:31:03 -0500

    Phase 2: project skeleton and basic tab structure

---
+++ RailRadar/Core/Services/Onboard/OnboardStatus.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardStatus.swift
+//  
+//
+//  Created by Dinesh on 7/30/26.
+//
+
+
+struct OnboardStatus {
+    enum State { case notOnboard, betweenStations, atStation(String), arrivingSoon(String) }
+    let state: State
+    let distanceRemaining: Double?
+    let etaToNextStation: Date?
+    let etaToDestination: Date?
+}
+
+protocol OnboardTrackingServiceProtocol {
+    func startTracking(journey: Journey)
+    func stopTracking()
+    var statusPublisher: AnyPublisher<OnboardStatus, Never> { get }
+}
---
+++ RailRadar/DesignSystem/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  
+//
+//  Created by Dinesh on 7/30/26.
+//
+
+import SwiftUI
+
+enum AppColors {
+    static let backgroundPrimary = Color("BackgroundPrimary", bundle: nil) // Define in asset catalog later
+    static let backgroundSecondary = Color("BackgroundSecondary", bundle: nil)
+    static let accent = Color("AccentColor", bundle: nil)
+}
